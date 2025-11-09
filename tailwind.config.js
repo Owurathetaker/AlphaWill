5432790b16995ae91269b152f042e338587300f1
Owurathetaker <opokujay45@gmail.com>
2025-11-09 01:16:54 +0000

    Update and rename tailwind.config.ts to tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./client/index.html",
+    "./client/src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: "var(--border)" // only if you use border-border
+      }
+    },
+  },
+  plugins: [],
+}
